dc0dc5febc856c42a2360987301b5b4481cac5fa
JeffPerk <jeff.perk87@gmail.com>
2016-08-08 17:00:28 -0600

    Added bootstrap css and js
    Added css and js files as well as created the front end form for the
    submission

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
   $curl = curl_init();
-  $q = 'dogs';
+  $q = $_GET['search'];
 
   curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://api.giphy.com/v1/gifs/search?q='.$q.'&api_key=dc6zaTOxFJmzC',
+    CURLOPT_URL => 'http://api.giphy.com/v1/gifs/search?q='. $q .'&api_key=dc6zaTOxFJmzC',
     CURLOPT_USERAGENT => 'Giphy Sample Request'
   ));
 
@@ -22,9 +22,55 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giphy App</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" charset="utf-8">
+    <link rel="stylesheet" href="css/style_main.css" charset="utf-8">
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
   </head>
   <body>
-    Test
+
+    <section class="header-container">
+      <div class="app-name">
+        GiphyApp
+      </div>
+      <div class="favorites-link">
+        Favorites
+      </div>
+    </section>
+    <section class="search-container">
+      <div class="title-container">
+        <div class="title-banner">
+          The Giphy App
+        </div>
+        <hr>
+      </div>
+      <div class="search-input-container">
+        <div class="suggestion-boxes">
+          <div class="suggestion-box">
+
+          </div>
+          <div class="suggestion-box">
+
+          </div>
+          <div class="suggestion-box">
+
+          </div>
+        </div>
+        <div class="input-container">
+          <div class="input-title">
+            <h4>Find Your Giphy!</h4>
+          </div>
+          <form class="input-form" action="index.php" method="post">
+            <input id="search-field" type="text" name="giphy" placeholder="Search For Giphy...">
+            <input id="search-button" class="btn btn-success" type="submit" name="submit" value="Search">
+          </form>
+        </div>
+      </div>
+    </section>
+
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
