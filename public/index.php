55500ae16241696db8e8bbc1319e25cc7e0c9eaa
JeffPerk <jeff.perk87@gmail.com>
2016-08-10 11:57:18 -0600

    made a public folder
    public folder container the index.php file. Also trying to get the new
    location page working

--- public/index.php
+++ public/index.php
@@ -1,21 +1,22 @@
 <?php
-  $curl = curl_init();
-  $q = $_GET['search'];
-
-  curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://api.giphy.com/v1/gifs/search?q='. $q .'&api_key=dc6zaTOxFJmzC',
-    CURLOPT_USERAGENT => 'Giphy Sample Request'
-  ));
-
-  $resp = curl_exec($curl);
-  // pre($resp);
-
-  curl_close($curl);
-
-  function pre($var) {
-   	echo sprintf('<pre>%s</pre>', print_r($var, true));
-  }
+//   $curl = curl_init();
+//   if (isset($_POST['submit'])) {
+//     dd($_POST);
+//     curl_setopt_array($curl, array(
+//       CURLOPT_RETURNTRANSFER => 1,
+//       CURLOPT_URL => 'http://api.giphy.com/v1/gifs/search?q='. $q .'&api_key=dc6zaTOxFJmzC',
+//       CURLOPT_USERAGENT => 'Giphy Sample Request'
+//     ));
+//
+//   $resp = curl_exec($curl);
+//   // pre($resp);
+//
+//   curl_close($curl);
+// }
+//
+//   function pre($var) {
+//    	echo sprintf('<pre>%s</pre>', print_r($var, true));
+//   }
 ?>
 
 <!DOCTYPE html>
