bf4c96d10886e7d644d103b8c8733c371cb5ef39
JeffPerk <jeff.perk87@gmail.com>
2016-08-19 17:13:23 -0600

    Urls now visible

--- public/giphy_results.php
+++ public/giphy_results.php
@@ -3,16 +3,22 @@
 if (isset($_POST['submit'])) {
   $curl = curl_init();
   $q = $_POST['giphy'];
+  if (ctype_space($q)) {
+    echo "I'm here";
+    // $search_q = str_replace(" ", "", $q);
+  }
+  // echo $search_q;
   curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => 'http://api.giphy.com/v1/gifs/search?q='. $q .'&api_key=dc6zaTOxFJmzC',
     CURLOPT_USERAGENT => 'Giphy Sample Request'
   ));
-  $data = curl_exec($curl);
+  $data= curl_exec($curl);
+  $data = json_decode($data, true);
   curl_close($curl);
 }
-// echo $data;
-
+  // echo "<pre>".print_r($data, true)."</pre>";
+  // exit;
  ?>
 <!DOCTYPE html>
 <html>
@@ -21,6 +27,9 @@ if (isset($_POST['submit'])) {
     <title>Giphy Results</title>
   </head>
   <body>
-    
+    <?php foreach ($data['data'] as $key => $value): ?>
+      <?php //echo "<pre>".print_r($value, true)."</pre>"; ?>
+      <?php echo "<img src='".$value['images']['fixed_height']['url']."'/>"; ?>
+    <?php endforeach; ?>
   </body>
 </html>
